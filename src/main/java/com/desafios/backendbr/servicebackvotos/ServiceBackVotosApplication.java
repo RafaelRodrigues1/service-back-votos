244adcf5668df143a518677a4576e0a01ac10444
Rafael Rodrigues da Silva <rafaelx7t@hotmail.com>
2024-07-03 07:11:46 -0300

    add project base

---
+++ src/main/java/com/desafios/backendbr/servicebackvotos/ServiceBackVotosApplication.java
@@ -0,0 +1,13 @@
+package com.desafios.backendbr.servicebackvotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBackVotosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceBackVotosApplication.class, args);
+    }
+
+}
